793ebe8d7c19aa3a5b356201c80a38559c105a80
mingyach <894886813@qq.com>
2026-04-19 16:55:04 +0000

    score

--- examples/industrial_data_pretraining/fun_asr_nano/generate_data.sh
+++ examples/industrial_data_pretraining/fun_asr_nano/generate_data.sh
@@ -7,9 +7,9 @@ cd "$(dirname "$0")"
 python mose/generate_severity_score.py \
     --input  dysar_data/train.jsonl \
     --output dysar_data/train_scored.jsonl \
-    --num_workers 2
+    --num_workers 4
 
 python mose/generate_severity_score.py \
     --input  dysar_data/val.jsonl \
     --output dysar_data/val_scored.jsonl \
-    --num_workers 2
+    --num_workers 4
